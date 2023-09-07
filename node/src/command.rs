64ed5050fc009b1af7d7626ad993b56ef8e989a4
Jorge Almeida <jorge.almeida@cvaconsultoria.com.br>
2023-09-07 19:35:19 -0300

    fix build

--- node/src/command.rs
+++ node/src/command.rs
@@ -4,8 +4,10 @@ use crate::{
 	cli::{Cli, Subcommand},
 	service,
 };
+
 use frame_benchmarking_cli::{BenchmarkCmd, ExtrinsicFactory, SUBSTRATE_REFERENCE_HARDWARE};
-use node_template_runtime::{Block, EXISTENTIAL_DEPOSIT};
+use lunes_runtime::constants::currency::*;
+use lunes_runtime::{Block};
 use sc_cli::SubstrateCli;
 use sc_service::PartialComponents;
 use sp_keyring::Sr25519Keyring;
@@ -160,7 +162,7 @@ pub fn run() -> sc_cli::Result<()> {
 							Box::new(TransferKeepAliveBuilder::new(
 								client.clone(),
 								Sr25519Keyring::Alice.to_account_id(),
-								EXISTENTIAL_DEPOSIT,
+								EXISTENTIAL_DEPOSIT_LUNES,
 							)),
 						]);
 
--- runtime/src/constants.rs
+++ runtime/src/constants.rs
@@ -4,8 +4,8 @@
 pub mod currency {
 	
     pub const UNIT: u128 = 100_000_000;
-    pub const MILLIUNIT: u128 = 1_000_000_000;
-    pub const MICROUNIT: u128 = 1_000_000;
+    pub const MILLIUNIT: u128 = 1_000_000;
+    pub const MICROUNIT: u128 = 100_000;
     pub const NANOUNIT: u128 = 1_000;
     pub const TOTAL_INITIAL_ISSUANCE_LUNES: u128 = 200_000_000 * UNIT;
     pub const INITIAL_COLLATOR_STAKING: u128 = 5_000 * UNIT;
