3bf9385d3d990be6c3474efa28529f6d73accaa2
Jorge Almeida <jorge.almeida@cvaconsultoria.com.br>
2023-10-10 16:37:54 -0300

    ajustes

---
+++ scripts/babe_chain.sh
@@ -0,0 +1,12 @@
+
+curl http://localhost:9933  -H "Content-Type:application/json;charset=utf-8" -d "@babe1"
+{
+    "jsonrpc":"2.0",
+    "id":1,
+    "method":"author_insertKey",
+    "params": [
+        "babe",
+        "bottom drive obey lake curtain smoke basket hold race lonely fit walk//Alice//1//babe",
+        "0xd43593c715fdd31c61141abd04a99fd6822c8558854ccde39a5684e7a56da27d"
+    ]
+}
