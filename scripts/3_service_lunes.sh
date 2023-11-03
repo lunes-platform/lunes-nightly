e7232aa1e40197aad3ca759916388d2f22981242
Jorge Almeida <jorgewra@gmail.com>
2023-11-03 18:07:58 -0300

    new node uplunes

--- scripts/3_service_lunes.sh
+++ scripts/3_service_lunes.sh
@@ -2,5 +2,5 @@
 cp lunes_bootnode.service  /etc/systemd/system/lunes_bootnode.service
 cp lunes_validator.service  /etc/systemd/system/lunes_validator.service
 sudo systemctl daemon-reload
-sudo systemctl start lunes_testnet_bootnode.service
-sudo systemctl start lunes_testnet_validator.service
+sudo systemctl start lunes_bootnode.service
+sudo systemctl start lunes_validator.service
